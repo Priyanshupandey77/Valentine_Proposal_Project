8375b4e4c940c5bc0b208fce60217c2493900133
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-19 00:12:20 +0530

    fixed cors

--- valentine-backend/src/server.js
+++ valentine-backend/src/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectedDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 connectedDB();
@@ -17,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("API is running!!");
 });
 
-app.use("/api/users", userRoutes );
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
